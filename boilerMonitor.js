cb081da1258f9c38184759798a2df0271508722b
Abraham Vallejos-Soto <125098951+AVSbeats@users.noreply.github.com>
2023-04-06 19:20:36 -0400

    Week 5 Done

---
+++ boilerMonitor.js
@@ -0,0 +1,4 @@
+// This will be use for later chapters 6 & 7
+// Already set up
+// Deleted the username & password script for easy navigation
+// Obtain the file saved in the folder file jQuery.AVS.Folder.Week5
